b2c699d84e06741565c336b4656f61c78bc61aa7
danielemolino <114595385+danielemolino@users.noreply.github.com>
2023-01-14 09:40:56 +0100

    Implementata la possibilità di avere più di una casella di partenza.
    In questa versione, il programma riconosce la presenza di più caselle di partenza, e calcola per ciascuna tutti i percorsi esistenti, che vengono stampati ciascuno su un'immagine con un colore diverso a seconda della partenza.

--- maze2.py
+++ maze2.py
@@ -1,34 +1,44 @@
 from collections import defaultdict
 from PIL import Image
 
-def dfs(maze, start, end):
-    # Crea una matrice per tracciare se una cella è stata visitata
-    visited = [[False for _ in range(len(maze[0]))] for _ in range(len(maze))]
-    # Crea una pila per tenere traccia dei nodi da visitare
-    stack = [start]
-    # Crea un dizionario per tenere traccia dei percorsi
-    paths = defaultdict(list)
-    paths[start] = []
-    # Fai un ciclo finché la pila non è vuota
+def find_all_paths(maze, start, end):
+    # Creiamo una pila vuota per tener traccia dei percorsi
+    stack = []
+    # Iniziamo con un percorso vuoto
+    stack.append([start])
+    # Creiamo una lista vuota per tener traccia dei percorsi completi
+    paths = []
+    # Continuiamo a cercare percorsi finché la pila non è vuota
     while stack:
-        node = stack.pop()
-        x, y = node
-        # Se il nodo corrente è il punto di arrivo, aggiungi il percorso corrente al dizionario dei percorsi
-        if node == end:
-            paths[end] = paths[node] + [end]
-            continue
-        # Marcamos la celula come visitata
-        visited[x][y] = True
-        # Visitiamo i nodi non visitati adiacenti
-        for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-            if is_valid_move(maze, x+dx, y+dy, visited):
-                stack.append((x+dx, y+dy))
-                paths[(x+dx, y+dy)] = paths[node] + [(x+dx, y+dy)]           
-    return paths[end]
+        # Prendiamo il primo percorso dalla pila
+        path = stack.pop()
+        # Otteniamo la posizione corrente dal percorso
+        curr_pos = path[-1]
+        # Se la posizione corrente è uguale alla posizione di arrivo, aggiungiamo il percorso alla lista dei percorsi completi
+        if curr_pos == end:
+            paths.append(path)
+        # Altrimenti, esploriamo le posizioni adiacenti
+        else:
+            # Per ogni posizione adiacente alla posizione corrente
+            for next_pos in get_adjacent_positions(maze, curr_pos):
+                # Se la posizione adiacente non è già stata visitata
+                if next_pos not in path:
+                    # Creiamo un nuovo percorso che include la posizione adiacente
+                    new_path = list(path)
+                    new_path.append(next_pos)
+                    # Aggiungiamo il nuovo percorso alla pila
+                    stack.append(new_path)
+    # Restituiamo tutti i percorsi completi
+    return paths
 
-def is_valid_move(maze, x, y, visited):
-    # Controlla se la mossa è valida (non fuori dai confini del labirinto e non è una parete)
-    return (0 <= x < len(maze)) and (0 <= y < len(maze[0])) and (maze[x][y] != "#") and (not visited[x][y])
+def get_adjacent_positions(maze, pos):
+    x, y = pos
+    adjacent_positions = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+    valid_positions = []
+    for i,j in adjacent_positions:
+        if i>=0 and j>=0 and i<len(maze) and j<len(maze[0]) and maze[i][j]=='.':
+            valid_positions.append((i,j))
+    return valid_positions
 
 def image_to_maze(filepath):
     # Apre l'immagine del labirinto
@@ -37,6 +47,7 @@ def image_to_maze(filepath):
     pixels = image.load()
     # Crea una matrice vuota per il labirinto
     maze = [] 
+    start = []
     # Scansiona i pixel dell'immagine per creare la matrice del labirinto
     # S = inizio, E = fine, # = muro, . = cammino
     for i in range(image.height):
@@ -48,27 +59,36 @@ def image_to_maze(filepath):
             elif pixel == (0, 0, 0): # Pixel nero
                 maze_row.append("#") # muro
             elif pixel == (255, 0, 0): # Pixel rosso
-                maze_row.append("E") # inizio
+                maze_row.append(".") # inizio
                 end = (i, j)
             elif pixel == (0, 255, 0): # Pixel verde
-                maze_row.append("S") # fine
-                start = (i, j)
+                maze_row.append(".") # fine
+                start.append((i, j))
+            else:
+                maze_row.append(".") # fine
         maze.append(maze_row)
     return maze, start, end
 
 
-def draw_path(filepath, path):
-    # Apre l'immagine del labirinto
-    image = Image.open(filepath)
-    pixels = image.load()
-    # Disegna il percorso sul labirinto
-    for x, y in path[1,(len(path)-1)]:
-        pixels[y, x] = (0, 0, 255) # Assegniamo un colore blu
-    # Salva l'immagine con il percorso
-    image.save("path_maze.tiff")
+def draw_path(filepath, paths,index):
+    colors = [(0,255,255),(255,0,255),(0,128,0),(128,0,128),(255,255,0),(192,192,192)]
+    name = filepath.split('.')[0] + '_start_' + str(index)
+    for i,path in zip(range(len(paths)),paths):
+        # Apre l'immagine del labirinto
+        image = Image.open(filepath)
+        pixels = image.load()
+        # Disegna il percorso sul labirinto
+        for x, y in path[1:(len(path)-1)]:
+            pixels[y, x] = colors[index] # Assegniamo un colore blu
+            # Salva l'immagine con il percorso
+        image.save(f'./Percorsi/{name}_path_{i+1}.tiff')
     
 # Esempio di utilizzo
-filepath = "maze.tiff"
+filepath = "maze3.tiff"
 maze, start, end = image_to_maze(filepath)
-path = dfs(maze, start, end)
-draw_path(drawpath, path)
+paths = []
+for i in range(len(start)):
+    paths.append(find_all_paths(maze, start[i], end))
+    draw_path("maze3.tiff", paths[i],i)
+
+
